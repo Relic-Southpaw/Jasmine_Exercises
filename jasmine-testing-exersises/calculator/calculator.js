9e1c0879a0b1a66eaf2a9d9288edc15a593eaa11
Relic <RobertSmithTheFourth@Outlook.com>
2022-07-12 08:57:59 -0400

    Worked out the testing on loan calculator

--- jasmine-testing-exersises/calculator/calculator.js
+++ jasmine-testing-exersises/calculator/calculator.js
@@ -1,16 +1,15 @@
+let monthly = 0;
+let values = {};
 window.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById("calc-form");
   if (form) {
     setupIntialValues();
     form.addEventListener("submit", function (e) {
-      values = [];
       update();
       e.preventDefault();
     });
   }
 });
-let values = [];
-let monthly = 0;
 
 function getCurrentUIValues() {
   return {
@@ -24,38 +23,43 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
-  let amount = 0;
-  let years = 0;
-  let rate = 0;
+  values = {
+    amount: 0,
+    years: 0,
+    rate: 0
+  };
   update()
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
-  values.push(Object.values(getCurrentUIValues()));
-  calculateMonthlyPayment();
+  values.amount = getCurrentUIValues().amount
+  values.years = getCurrentUIValues().years
+  values.rate = getCurrentUIValues().rate
+  calculateMonthlyPayment(values);
+  updateMonthly()
 }
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
-function calculateMonthlyPayment() {
-  let p = Object.values(getCurrentUIValues())[0];
-  let yTot = Object.values(getCurrentUIValues())[1];
-  let yRate = Object.values(getCurrentUIValues())[2];
+function calculateMonthlyPayment(values) {
+  const p = values.amount;
+  const yTot = values.years;
+  const yRate = values.rate;
   let i = (yRate / 100) / 12;
   let n = yTot * 12
   let longPay = ((p * i) / (1 - Math.pow((1 + i), -n)));
   let eachMonth = longPay.toFixed(2);
   monthly = eachMonth;
-  updateMonthly();
-  console.log(monthly)
+  return monthly;
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly() {
   monthly = +monthly || 0;
+  monthly = monthly.toFixed(2);
   const mPay = `the monthly payment will be $${monthly} a month`;
   const mPayment = document.getElementById("monthly-payment");
   mPayment.innerText = mPay;
